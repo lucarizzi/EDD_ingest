f2539be66a310ffeda867bf80973de63f4ae5f44
Luca Rizzi <lrizzi@keck.hawaii.edu>
2012-08-27 14:44:03 -1000

    moved config parameters to a config file

---
+++ EDD_config.py
@@ -0,0 +1,12 @@
+
+# database configuration files
+
+host = "localhost"
+user = "distance"
+passwd = "CosmicFlu2"
+database = "EDDsDB"
+
+# directory containing the data bar files
+
+bar_files = "/Volumes/DataDisk1Tb/Work/EDD/EDD_Database/bar_files"
+
